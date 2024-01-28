49cf3cad37686700e110fcfe701d634cefe86184
PrathmTayade <122280972+PrathmTayade@users.noreply.github.com>
2024-01-28 17:00:42 +0530

    add userlist
    server sided fetching

---
+++ frontend/src/components/CardComponent.tsx
@@ -0,0 +1,14 @@
+import { User } from "@/utils/types";
+import React from "react";
+
+const CardComponent: React.FC<{ card: User }> = ({ card }) => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-2 mb-2 hover:bg-gray-100">
+      <div className="text-sm text-gray-600">Id: {card.id}</div>
+      <div className="text-lg font-semibold text-gray-800">{card.name}</div>
+      <div className="text-md text-gray-700">{card.email}</div>
+    </div>
+  );
+};
+
+export default CardComponent;
